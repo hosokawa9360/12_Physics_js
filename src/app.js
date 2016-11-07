42c4adfb9fbf7c1f5277dfe4c36d547a5a606d09
Junya Hosokawa <hosokawa@morijyobi.com>
2016-11-07 10:50:04 +0900

    トーテム追加

--- src/app.js
+++ src/app.js
@@ -10,13 +10,20 @@ var gameScene = cc.Scene.extend({
 var game = cc.Layer.extend({
     init:function () {
         this._super();
+        var size = cc.director.getWinSize();
+// 背景
         var backgroundLayer = cc.LayerGradient.create(cc.color(0xdf,0x9f,0x83,255),
         cc.color(0xfa,0xf7,0x9f,255));
         this.addChild(backgroundLayer);
+// 地面
         var ground =  cc.Sprite.create(res.ground_png);
-        var size = cc.director.getWinSize();
         ground.setPosition(size.width/2, 10);
         this.addChild(ground);
+// トーテム
+        var totem =  cc.Sprite.create(res.totem_png);
+        totem.setPosition(size.width/2, size.height/2);
+        this.addChild(totem);
+
         this.scheduleUpdate();
 
     },
